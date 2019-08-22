3c005ec4041ade4155a61fdbdadbcbc2c6a97da2
Luan94 <luanvilas0@gmail.com>
2019-08-22 19:58:03 -0300

    Backend1
    Desenvolvimento do Backend 22/08

---
+++ BackEnd/src/server.js
@@ -0,0 +1,9 @@
+//Livraria express para rotas
+const express = require ('express');
+ //Função que quando chamada cria um servidor , uma nova porta de entrada e retorno de respostas 
+const server = express ();
+//Endereço '/' e função 
+server.get('/', (req, res) => {
+    return res.send ('Hello World');}); 
+//Porta que o servidor vai atender
+server.listen (3333); 
