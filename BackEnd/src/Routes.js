d0aa48102d676079541d77147c5e1179d4a2e3ca
Luan94 <luanvilas0@gmail.com>
2019-08-27 01:18:36 -0300

    Correção Backend2 GIT

--- BackEnd/src/Routes.js
+++ BackEnd/src/Routes.js
@@ -1,7 +1,5 @@
 //Parte voltada apenas para tratação de rotas
 
-const express = ('Require express'); //importando livaria express
-const routes = express.router() //função que pertence ao 'Express" é uma função específica que cria objetos para rotas
 const express = require('express'); //importando livaria express
 const routes = express.Router(); //função que pertence ao 'Express" é uma função específica que cria objetos para rotas
 
--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -10,12 +10,10 @@ const express = require ('express');
 const server = express ();
 
 //Importar 'Rotas' de Routes.js
-const routes = require ('./Routes') 
-const routes = require ('./Routes');
+const routes = require('./Routes').default;
 /*a sintaxe para importar um arquivo que não é um módulo feito por terceiros
  é preciso indicar o caminho do arquivo nesse caso /. pois está na mesma pasta*/
 
- server.use(routes)//use geralmente é utilizado para configurações que está em outro arquivo funciona como se fosse add um plugin
  server.use(routes);//use geralmente é utilizado para configurações que está em outro arquivo funciona como se fosse add um plugin
 
 
