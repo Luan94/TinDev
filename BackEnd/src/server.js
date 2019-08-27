84f14ec92714f6033c0c5ee592a7d9b6017eebcd
Luan94 <luanvilas0@gmail.com>
2019-08-26 23:30:10 -0300

    Update server.js

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -1,9 +1,13 @@
+/*abra a pasta do projeto antes de começar!!
+/dê load no terminal integrado (ctrl + P toggle terminal).
+salve e carregue node server.js (nome desse arquivo) para toda alteração feita no arquivo*/
+
 //Livraria express para rotas
 const express = require ('express');
  //Função que quando chamada cria um servidor , uma nova porta de entrada e retorno de respostas 
 const server = express ();
 //Endereço '/' e função 
 server.get('/', (req, res) => {
-    return res.send ('Hello World');}); //res.send resposta envia string 'Hello World'
+    return res.send (`Hello ${req.query.name}`);}); //res.send envia string 'Hello $querynaurl name=alguma coisa'
 //Porta que o servidor vai atender
 server.listen (3333); 
